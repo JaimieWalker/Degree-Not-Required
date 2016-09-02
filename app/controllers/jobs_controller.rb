f6c28dcbe1a17cb746ba2c42904e3b1003d40793
JaimieWalker <jaimie.walker90@gmail.com>
2016-09-01 23:51:31 -0400

    Able to get a json of data and time to change database

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,8 +2,10 @@ class JobsController < ApplicationController
 	
 	def index
 		indeed = Indeed.new(Rails.application.secrets.INDEED_PUBLISHER_KEY)
-		indeed_response = Indeed.api_request(indeed.constructUrl(params))
-		binding.pry
+		indeed_response = Indeed.api_request(indeed.construct_url(params))
+		@indeed_response = indeed.model_data(indeed_response)
+		render json: @indeed_response
+		
 
 	end
 end
--- lib/assets/indeed.rb
+++ lib/assets/indeed.rb
@@ -3,7 +3,10 @@ require "net/http"
 require "json"
 class Indeed
 	attr_accessor :api_key,:url
+	attr_reader :data
+
 	def initialize(api_key)
+
 		@api_key = api_key
 		@options = Url_Options.new(2,"json")
 		@options[:latlong] = 1
@@ -18,13 +21,13 @@ class Indeed
 	
 # Params is passed in from jobs controller in from the front end.
 # Params is available at the controller level
-	def constructUrl(params)
+	def construct_url(params)
 		url = "http://api.indeed.com/ads/apisearch?publisher=#{api_key}&format=json"
 		@options[:q] = params['query']
-		url += concat_struct_to_url
+		url << concat_struct_to_url
 		return url
 	end
-
+# takes the string for the urls api call and returns a raw json file
 	def self.api_request(url)
 		return JSON.parse(Net::HTTP.get(URI(url)))
 	end
@@ -34,10 +37,16 @@ class Indeed
 		options = ""
 		 @options.each_pair{ |key,value|
 			next if value == nil
-			options += "&#{key.to_s}=#{URI.escape(value.to_s)}"
+			options << "&#{key.to_s}=#{URI.escape(value.to_s)}"
 		}
 		return options
 	end
-	
+
+# Models the data from the json
+	def model_data(json)
+			
+		binding.pry
+
+	end
 	
 end
