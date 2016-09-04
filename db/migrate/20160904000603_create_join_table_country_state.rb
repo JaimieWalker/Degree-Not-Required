e9bbb3db9954a1b339d0d1245841315816346c08
JaimieWalker <jaimie.walker90@gmail.com>
2016-09-03 21:26:40 -0400

    made database, need to fix it up

--- db/migrate/20160904000603_create_join_table_country_state.rb
+++ db/migrate/20160904000603_create_join_table_country_state.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableCountryState < ActiveRecord::Migration[5.0]
   def change
     create_join_table :countries, :states do |t|
-      # t.index [:country_id, :state_id]
-      # t.index [:state_id, :country_id]
+      t.index [:country_id, :state_id]
+      t.index [:state_id, :country_id]
     end
   end
 end
--- db/migrate/20160904005514_create_join_table_company_job.rb
+++ db/migrate/20160904005514_create_join_table_company_job.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableCompanyJob < ActiveRecord::Migration[5.0]
   def change
     create_join_table :companies, :jobs do |t|
-      # t.index [:company_id, :job_id]
-      # t.index [:job_id, :company_id]
+      t.index [:company_id, :job_id]
+      t.index [:job_id, :company_id]
     end
   end
 end
