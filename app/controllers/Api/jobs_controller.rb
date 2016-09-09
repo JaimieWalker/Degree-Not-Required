075dfaf4298ac72fbebafd98444b184fe15e6499
JaimieWalker <jaimie.walker90@gmail.com>
2016-09-09 12:18:09 -0400

    tested with different requests for optimization

--- app/controllers/Api/jobs_controller.rb
+++ app/controllers/Api/jobs_controller.rb
@@ -1,21 +1,18 @@
 class Api::JobsController < ApplicationController
+	# Guards against calls to the api, before action is faster than respond_to
 	before_action :ensure_json_request  
-
 
 	def index
 		indeed = Indeed.new(Rails.application.secrets.INDEED_PUBLISHER_KEY)
 		indeed.construct_url(params)
 		json = indeed.api_request
 		results = indeed.fill_page_with_results(json)
-		render json: results
-	end
-
-	def show
-		
+	    render :json => results	
 	end
 
 	def ensure_json_request  
-	  return if request.format == :json
-	  head :reset_content  
+	  return if request.format == :json  
+	  head :reset_content
 	end
+
 end
