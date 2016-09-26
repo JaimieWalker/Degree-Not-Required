90e676caa652066f02581b175d2a6381a5a41d3f
JaimieWalker <jaimie.walker90@gmail.com>
2016-09-26 12:32:25 -0400

    more style changes

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,6 +1,5 @@
 var app = angular.module("Degree_Not_Required", 
 	['ngResource','ui.router','templates']);
-
 app.config(function($stateProvider, $urlRouterProvider,$locationProvider){
 	$locationProvider.html5Mode(true);
     $stateProvider
