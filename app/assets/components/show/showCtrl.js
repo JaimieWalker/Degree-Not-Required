555818183a4c7d5b7bbbc413cf8a4a912e37cfcf
JaimieWalker <jaimie.walker90@gmail.com>
2016-10-02 00:58:51 -0400

    need to style show page

--- app/assets/components/show/showCtrl.js
+++ app/assets/components/show/showCtrl.js
@@ -1,9 +1,12 @@
 angular.module('Degree_Not_Required')
-.controller('showCtrl', function($scope,$rootScope) {
+.controller('showCtrl', function($scope,$rootScope,$sce) {
 	$scope.jobResults = $rootScope.jobResults;
 	$scope.currentPage = $rootScope.currentPage;
 	$scope.currentJob = $rootScope.currentJob;
-	
+
 	$scope.job = $scope.jobResults[$scope.currentPage][$scope.currentJob];
 
+	$scope.trustAsHtml = function(html) {
+      return $sce.trustAsHtml(html);
+    }
 });
